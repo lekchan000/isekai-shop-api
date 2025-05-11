4acb3e08cdd8a2da43f412f61322cce07667c93e
Suriya Thongsi <lekchan000@gmail.com>
2025-05-11 21:09:11 +0700

    Implement migration functions for Player, Admin, Item, PlayerCoin, Inventory, and PurchaseHistory entities; add PlayerCoin entity definition

--- database/migration/migration.go
+++ database/migration/migration.go
@@ -5,11 +5,43 @@ import (
 
 	"github.com/lekchan000/isekai-shop-api/config"
 	"github.com/lekchan000/isekai-shop-api/database"
+	"github.com/lekchan000/isekai-shop-api/entities"
 )
 
 func main() {
 	conf := config.ConfigGetting()
 	db := database.NewPostgresDatabase(conf.Database)
 
-	fmt.Println(db.ConnectionGetting())
+	// Run migrations
+	playerMigration(db)
+	adminMigration(db)
+	itemMigration(db)
+	playerCoinMigration(db)
+	inventoryMigration(db)
+	purchaseHistoryMigration(db)
+	fmt.Println("Migrations successfully.")
+}
+
+func playerMigration(db database.Database) {
+	db.ConnectionGetting().Migrator().CreateTable(&entities.Player{})
+}
+
+func adminMigration(db database.Database) {
+	db.ConnectionGetting().Migrator().CreateTable(&entities.Admin{})
+}
+
+func itemMigration(db database.Database) {
+	db.ConnectionGetting().Migrator().CreateTable(&entities.Item{})
+}
+
+func playerCoinMigration(db database.Database) {
+	db.ConnectionGetting().Migrator().CreateTable(&entities.PlayerCoin{})
+}
+
+func inventoryMigration(db database.Database) {
+	db.ConnectionGetting().Migrator().CreateTable(&entities.Inventory{})
+}
+
+func purchaseHistoryMigration(db database.Database) {
+	db.ConnectionGetting().Migrator().CreateTable(&entities.PurchaseHistory{})
 }

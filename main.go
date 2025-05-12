6467061c5d5f686b4d4a93e9f26810bd40a5dea8
Suriya Thongsi <lekchan000@gmail.com>
2025-05-12 19:33:14 +0700

    Refactor main function: initialize configuration, database connection, and server instance

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/lekchan000/isekai-shop-api/config"
+	"github.com/lekchan000/isekai-shop-api/database"
+	"github.com/lekchan000/isekai-shop-api/server"
+)
 
+func main() {
+	conf := config.ConfigGetting()
+	db := database.NewPostgresDatabase(conf.Database)
+	server := server.NewEchoServer(conf, db.ConnectionGetting())
+
+	server.Start()
 }

bef5d28d51304b28d73892320df15a4cd304db34
Suriya Thongsi <lekchan000@gmail.com>
2025-05-14 22:35:05 +0700

    Fix typo in item struct: change IsAchieve to IsArchive

--- entities/item.go
+++ entities/item.go
@@ -14,7 +14,7 @@ type (
 		Description string    `gorm:"type:varchar(128);not null;"`
 		Picture     string    `gorm:"type:varchar(256);not null;"`
 		Price       uint      `gorm:"not null;"`
-		IsAchieve   bool      `gorm:"not null;default:false;"`
+		IsArchive   bool      `gorm:"not null;default:false;"`
 		CreatedAt   time.Time `gorm:"not null;autoCreateTime;"`
 		UpdatedAt   time.Time `gorm:"not null;autoUpdateTime;"`
 	}

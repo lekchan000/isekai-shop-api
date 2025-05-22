c28ce20147294471de87939df2b4a07e8f34a9d8
Suriya Thongsi <lekchan000@gmail.com>
2025-05-22 18:31:24 +0700

    Refactor error handling to pass error objects instead of strings in custom.Error calls

--- pkg/itemManaging/controller/itemManagingControllerImpl.go
+++ pkg/itemManaging/controller/itemManagingControllerImpl.go
@@ -24,13 +24,13 @@ func (c *itemManagingControllerImpl) Creating(pctx echo.Context) error {
 	customEchoRequest := custom.NewCustomEchoRequest(pctx)
 
 	if err := customEchoRequest.Bind(itemCreatingReq); err != nil {
-		return custom.Error(pctx, http.StatusBadRequest, err.Error())
+		return custom.Error(pctx, http.StatusBadRequest, err)
 	}
 
 	item, err := c.itemManagingService.Creating(itemCreatingReq)
 
 	if err != nil {
-		return custom.Error(pctx, http.StatusInternalServerError, err.Error())
+		return custom.Error(pctx, http.StatusInternalServerError, err)
 	}
 
 	return pctx.JSON(http.StatusCreated, item)
@@ -39,7 +39,7 @@ func (c *itemManagingControllerImpl) Creating(pctx echo.Context) error {
 func (c *itemManagingControllerImpl) Editing(pctx echo.Context) error {
 	itemID, err := c.getItemID(pctx)
 	if err != nil {
-		return custom.Error(pctx, http.StatusBadRequest, err.Error())
+		return custom.Error(pctx, http.StatusBadRequest, err)
 	}
 
 	itemEditingReq := new(_itemManagingModel.ItemEditingReq)
@@ -47,12 +47,12 @@ func (c *itemManagingControllerImpl) Editing(pctx echo.Context) error {
 	customEchoRequest := custom.NewCustomEchoRequest(pctx)
 
 	if err := customEchoRequest.Bind(itemEditingReq); err != nil {
-		return custom.Error(pctx, http.StatusBadRequest, err.Error())
+		return custom.Error(pctx, http.StatusBadRequest, err)
 	}
 
 	item, err := c.itemManagingService.Editing(itemID, itemEditingReq)
 	if err != nil {
-		return custom.Error(pctx, http.StatusInternalServerError, err.Error())
+		return custom.Error(pctx, http.StatusInternalServerError, err)
 	}
 	return pctx.JSON(http.StatusOK, item)
 }
@@ -60,10 +60,10 @@ func (c *itemManagingControllerImpl) Editing(pctx echo.Context) error {
 func (c *itemManagingControllerImpl) Achiving(pctx echo.Context) error {
 	itemID, err := c.getItemID(pctx)
 	if err != nil {
-		return custom.Error(pctx, http.StatusBadRequest, err.Error())
+		return custom.Error(pctx, http.StatusBadRequest, err)
 	}
 	if err := c.itemManagingService.Archiving(itemID); err != nil {
-		return custom.Error(pctx, http.StatusInternalServerError, err.Error())
+		return custom.Error(pctx, http.StatusInternalServerError, err)
 	}
 	return pctx.NoContent(http.StatusNoContent)
 }
